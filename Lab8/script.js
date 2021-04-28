b83b52bffd07625333b8a4f8f947c6c4ad4c703a
Mxkxk <maksim.fedoryak@gmail.com>
2021-04-28 08:36:08 +0300

    Update script.js

--- Lab8/script.js
+++ Lab8/script.js
@@ -1,10 +1,11 @@
 var script = {};
 script.names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
-
-for (var i = 0; i < script.names.length; i++) {
-  if (script.names[i].toLowerCase()[0] == "j") {
-    SpeakGoodBye.speak(script.names[i]);
-  } else {
-    SpeakHello.speak(script.names[i]);
-  }
-}
+(function(){
+	for (var i = 0; i < script.names.length; i++) {
+	  if (script.names[i].toLowerCase()[0] == "j") {
+	    SpeakGoodBye.speak(script.names[i]);
+	  } else {
+	    SpeakHello.speak(script.names[i]);
+	  }
+	}
+})();
