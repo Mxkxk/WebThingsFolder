af4af3d78452979b0ee5e2c0d14950c4e8f01ee0
Mxkxk <maksim.fedoryak@gmail.com>
2021-05-19 17:01:47 +0300

    Update IIFE

--- Lab8/SpeakGoodBye.js
+++ Lab8/SpeakGoodBye.js
@@ -1,9 +1,13 @@
-var SpeakGoodBye = {
-	speakWord: "Good Bye",
-	speak: function speak(name) {
-	 	console.log(SpeakGoodBye.speakWord + " " + name);
-	},
-	power: function power(name) {
+var SpeakGoodBye = (function(){
+	const speakWord = "Good Bye"
+	function speak(name) {
+	 	console.log(speakWord + " " + name);
+	}
+	function power(name) {
 	 	console.log(name + " has enough power");
 	}
-}
+	return{
+		speak : speak,
+		power : power		
+	}	
+})();
--- Lab8/SpeakHello.js
+++ Lab8/SpeakHello.js
@@ -1,9 +1,13 @@
-var SpeakHello = {
-	speakWord: "Hello",
-	speak: function (name) {		
- 		console.log(SpeakHello.speakWord + " " + name);
- 	},
- 	power: function power(name) {
-	 	console.log(name + " hasn`t enough power");
+var SpeakHello = (function(){
+	const speakWord = "Hello"
+	function speak(name) {		
+ 		console.log(speakWord + " " + name);
+ 	}
+ 	function power(name) {
+	 	console.log(name + " hasn`t enough power");	 	
 	}
-}
+	return {		
+	 	speak : speak,
+	 	power : power
+	}
+})();
